1e88ec37a4d796bee29dc403345bb737694b5684
Dev <devbin404@gmail.com>
2023-01-08 15:19:31 +0330

    add profiles and user-profile

--- devsearch/users/models.py
+++ devsearch/users/models.py
@@ -7,6 +7,8 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True, blank=True)
     email = models.EmailField(max_length=200, blank=True, null=True)
+    username = models.CharField(max_length=200, null=True, blank=True)
+    location = models.CharField(max_length=200, null=True, blank=True)
     short_info = models.CharField(max_length=200, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
     profile_image = models.ImageField(null=True, blank=True, 
@@ -21,3 +23,17 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user.username) 
+
+
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile, null=True, blank=True,
+                              on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)   
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
+
